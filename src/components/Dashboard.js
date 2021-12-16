969ec04c02ff40b511dceacb2006df86cc483403
Tharsikan <thar.navaratnam@gmail.com>
2021-12-16 14:38:58 -0500

    add loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
-  }
-}
+    if (this.state.loading) return <Loading />;
+
+    return (
+      <main className={dashboardClasses} />
+    );
+  };
+};
 
 export default Dashboard;
